e04cdd79d43431fb1b8aa8d104b377e566b01ac0
decanus <dean.eigenmann@icloud.com>
2019-07-06 23:00:29 -0400

    using peerid

--- node/node.go
+++ node/node.go
@@ -1,12 +1,16 @@
 // Package node contains a HOPR node.
 package node
 
+import (
+	"github.com/libp2p/go-libp2p-core/peer"
+)
+
 // Node implements the HOPR node.
 type Node struct {
 
 }
 
 /// SendMessage is used to send a message over HOPR.
-func (n *Node) SendMessage(msg []byte, destination string) error {
+func (n *Node) SendMessage(msg []byte, destination peer.ID) error {
 	return nil
 }
