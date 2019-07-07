230ca25cbed54cb4fa5a1bd1552c16fe5409f424
decanus <dean.eigenmann@icloud.com>
2019-07-06 22:53:28 -0400

    started working on node

---
+++ node/node.go
@@ -0,0 +1,12 @@
+// Package node contains a HOPR node.
+package node
+
+// Node implements the HOPR node.
+type Node struct {
+
+}
+
+/// SendMessage is used to send a message over HOPR.
+func (n *Node) SendMessage(msg []byte, destination string) error {
+	return nil
+}
